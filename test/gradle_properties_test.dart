3182898002eb1fc187f756a8f16697b5c23b5b2c
BirjuVachhani <brvachhani@gmail.com>
2020-08-19 17:58:31 +0530

    :white_check_mark: improve code coverage

--- test/gradle_properties_test.dart
+++ test/gradle_properties_test.dart
@@ -4,7 +4,7 @@ import 'package:gradle_properties/gradle_properties.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('read from file test', () {
+  group('library test', () {
     final testDataFile = File(Platform.isWindows
         ? 'test\test_data.properties'
         : 'test/test_data.properties');
@@ -31,6 +31,26 @@ void main() {
       expect(properties['age'], '24');
     });
 
+    test('empty file test', () async {
+      final properties =
+          await GradleProperties.fromPath('test/empty_test_data.properties');
+      expect(properties, null);
+    });
+
+    test('load from map test', () async {
+      final properties = await GradleProperties.fromMap({
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'john@doe.com',
+        'age': '24',
+      });
+      expect(properties['first_name'], 'John');
+      expect(properties['last_name'], 'Doe');
+      expect(properties['email'], 'john@doe.com');
+      expect(properties['age'], '24');
+      expect(() async => await properties.save(), throwsException);
+    });
+
     test('save method test', () async {
       final properties = await GradleProperties.fromPath(testFile.path);
       expect(properties['first_name'], 'Birju');
@@ -53,6 +73,22 @@ void main() {
       final properties = await testFile.readProperties();
       expect(properties.contains('first_name'), true);
       expect(properties.contains('abcd'), false);
+    });
+
+    test('getters tests', () async {
+      final properties = await testFile.readProperties();
+      expect(properties.keys.contains('first_name'), true);
+      expect(properties.values.contains('Birju'), true);
+      expect(properties.keys.contains('address'), false);
+      properties.putIfAbsent('address', 'Ahmedabad');
+      expect(properties.keys.contains('address'), true);
+      properties
+          .addAll({'github': 'birjuvachhani', 'twitter': 'birjuvachhani'});
+      expect(properties.contains('github'), true);
+      expect(properties.contains('twitter'), true);
+      expect(properties['twitter'], 'birjuvachhani');
+      expect(properties['github'], 'birjuvachhani');
+      expect(properties.properties['last_name'], 'Vachhani');
     });
 
     tearDown(testFile.deleteSync);
